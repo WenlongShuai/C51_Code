776a282b55ae255d219bd04ad99f629a7665259a
DESKTOP-CTA1DA5\29610 <2961070026@qq.com>
2024-06-16 23:53:57 +0800

    no message

--- code/DS1302实时时钟_240614/realTimeClock.c
+++ code/DS1302实时时钟_240614/realTimeClock.c
@@ -3,17 +3,20 @@
 
 #include "DS1302.h"
 #include "LCD1602.h"
+#include "keyScanAdjustmentTime.h"
 
 uint hexTransformDec(uchar hex);
+
+//uchar time[10] = {0};
+//uchar week[7] = {0};
+uchar date[12] = {0};
 
 char code *weekList[] = {"Mon. ", "Tues.", "Wed. ", "Thur.", "Fri. ", "Sta. ", "Sun. "}; 
 
 int main()
 {
 	uchar dat = 0;
-	char time[10] = {0};
-	char week[7 ] = {0};
-	char date[12] = {0};
+	uchar key = 0;
 	
 	DS1302Init();
 	lcd1602Init();
@@ -21,15 +24,32 @@ int main()
 	while(1)
 	{
 		readTime();
-	
-		//序列化
-		sprintf(time, "%.2d:%.2d:%.2d", hexTransformDec(ds1302Read[2]), hexTransformDec(ds1302Read[1]),hexTransformDec(ds1302Read[0]));
-		sprintf(week, "%s", weekList[hexTransformDec(ds1302Read[5])-1]);
-		sprintf(date, "20%.2d-%.2d-%.2d", hexTransformDec(ds1302Read[6]), hexTransformDec(ds1302Read[4]),hexTransformDec(ds1302Read[3]));
+
+		rtc[0] = hexTransformDec(ds1302Read[0]);  //秒
+		rtc[1] = hexTransformDec(ds1302Read[1]); //分钟
+		rtc[2] = hexTransformDec(ds1302Read[2]);  //小时
+		rtc[5] = hexTransformDec(ds1302Read[5]);   //星期
+		rtc[3] = hexTransformDec(ds1302Read[3]);   //天
+		rtc[4] = hexTransformDec(ds1302Read[4]);  //月份
+		rtc[6] = hexTransformDec(ds1302Read[6]);  //年
 		
-		lcdDisplatString(1, 1, date);
-		lcdDisplatString(12, 1, week);
-		lcdDisplatString(1, 2, time);
+		key = detectKey();  //先判断是否有键按下，如果没键按下则不管
+		if(key == 1)
+		{
+			adjustmentTime(key);
+		}
+		else
+		{
+			//序列化
+			sprintf(date, "20%.2d-%.2d-%.2d", hexTransformDec(ds1302Read[6]), hexTransformDec(ds1302Read[4]),hexTransformDec(ds1302Read[3]));
+			lcdDisplatString(1, 1, date);
+			sprintf(date, "%s", weekList[hexTransformDec(ds1302Read[5])-1]);
+			lcdDisplatString(12, 1, date);
+			sprintf(date, "%.2d:%.2d:%.2d", hexTransformDec(ds1302Read[2]), hexTransformDec(ds1302Read[1]),hexTransformDec(ds1302Read[0]));
+			lcdDisplatString(1, 2, date);
+			
+		}
+
 	}
 	return 0;
 }
